68f24e720e7bb87659a878f0a27d45e68ecaf139
BT_3W2_32 <wendyz34@nycstudents.net>
2022-11-23 09:27:54 -0500

    print

--- src/Budget.java
+++ src/Budget.java
@@ -1,24 +1,41 @@
 import java.util.Scanner;
-class Runner{
+public class Budget{
 
-    private double totalIncome;
+    private double totalBill;
     private double savings;
-    private double spendings;
-    public Runner(double totalIncome, double savings){
-        this.totalIncome = totalIncome;
+    private double categories;
+    public Budget(double totalBill, double savings){
+        this.totalBill= totalBill;
         this.savings = savings;
-        // this.spendings = spendings;
+        this.categories = categories;
+
     }
-    public double operations(){
-        double left = totalIncome-savings;
+    public double amountLeft(){
+        double left = (totalBill-savings);
         Scanner scan = new Scanner(System.in);
+        return left;
+    }
+    /*public double Categories(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How much categories of other spending do you want(put int): ");
+        double otherspendings = scan.nextDouble();
+        return Categories();
+    }*/
+    /*public double nameCategories(){
+        for(int i =1; i==otherspendings;i++) {
+            System.out.println("Name your categtories: ");
+            categories = scan.nextInt();
+        }
+    }*/
 
 
+
+    public double roundedToHundredth(double toRound){
+        return Math.round(toRound*100)/100.000;
 
     }
-    public double spend(){
 
-    }
+
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,24 @@ class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean done = false;
-        System.out.println("Hello, this is a bugeting appplication");
+        System.out.println("Hello, this is a budgeting application. You'll be asked a series of question when you're done type done. ");
         System.out.println("What is your monthly earning?");
         double totalBill = scan.nextDouble();
         System.out.println("How much are you planning to save? ");
         double savings = scan.nextDouble();
-        Runner original = new Runner(totalIncome, savings);
+        Budget original = new Budget(totalBill, savings);
+        System.out.println("Amount left for other spendings: " + original.amountLeft());
+        //double numCat = original.Categories();
+        System.out.println("Num of categories: ");
+        double otherspendings = scan.nextDouble();
+            for(int i =1; i<=otherspendings;i++) {
+                String nameC = scan.nextLine();
+                System.out.println("Name your categtories(type in format:name, amount planned): ");
+                System.out.println(nameC);
+            }
+        if (done==true){
+            System.out.println("Application done");
+        }
+
     }
 }
