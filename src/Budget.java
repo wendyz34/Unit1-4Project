3c9e431dcff69fa1dbc66ef5d4ebd182ecc9f257
wendy <wendyz34@nycstudents.net>
2022-11-22 19:52:01 -0500

    print

--- src/Budget.java
+++ src/Budget.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
 class Runner{
-    private double totalBill;
+
+    private double totalIncome;
     private double savings;
     private double spendings;
-    public Runner(double totalBill, double savings){
-        this.totalBill = totalBill;
+    public Runner(double totalIncome, double savings){
+        this.totalIncome = totalIncome;
         this.savings = savings;
         // this.spendings = spendings;
     }
-    public void operations(){
+    public double operations(){
+        double left = totalIncome-savings;
+        Scanner scan = new Scanner(System.in);
+
+
 
     }
     public double spend(){
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        boolean done = false;
         System.out.println("Hello, this is a bugeting appplication");
         System.out.println("What is your monthly earning?");
         double totalBill = scan.nextDouble();
         System.out.println("How much are you planning to save? ");
         double savings = scan.nextDouble();
-        Runner original = new Runner(totalBill, savings);
+        Runner original = new Runner(totalIncome, savings);
     }
 }
