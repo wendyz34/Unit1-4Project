fc85bf1c95798d7602bcf0768d6a0e6846628f8e
wendy <wendyz34@nycstudents.net>
2022-11-23 20:56:12 -0500

    print

--- src/Budget.java
+++ src/Budget.java
@@ -1,24 +1,66 @@
 import java.util.Scanner;
-class Runner{
+public class Budget{
 
-    private double totalIncome;
+    private double totalBill;
     private double savings;
-    private double spendings;
-    public Runner(double totalIncome, double savings){
-        this.totalIncome = totalIncome;
+
+    private int numCategories;
+    private String categories;
+    public Budget(double totalBill, double savings){
+        this.totalBill= totalBill;
         this.savings = savings;
-        // this.spendings = spendings;
+        this.categories = categories;
+        this.numCategories = numCategories;
+
     }
-    public double operations(){
-        double left = totalIncome-savings;
+    public double amountLeft(){
+        double left = (roundedToHundredth(totalBill-savings));
         Scanner scan = new Scanner(System.in);
-
-
+        if (left<0){
+            System.out.println("Over your monthly wage. Rerun to restart");
+        }else if(left==0){
+            System.out.println("Saving Everything");
+        }else{
+            return left;
+        }
+        return left;
+    }
+    public String Categories(){
+        categories = "";
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Num of categories: ");
+        int numCategories = scan.nextInt();
+        for(int i =0; i<=numCategories;i++) {
+            String nameC = scan.nextLine();
+            System.out.println("Name your categtories: ");
+            categories+= nameC +"\n";
+        }
+        System.out.println(categories);
+        return categories;
 
     }
-    public double spend(){
+    public void otherSpendings(){
+        Scanner scan = new Scanner(System.in);
+        double total = 0;
+        for (int x =1;x<=numCategories;x++){
+            System.out.println("Enter your appromiate amount for category" + " " +  x);
+             total += scan.nextDouble();
+        }
+        System.out.println(total);
+    }
+    public void end(){
+        System.out.println("Remainding: ");
+    }
+
+
+
+
+
+    public double roundedToHundredth(double toRound){
+        return Math.round(toRound*100)/100.000;
 
     }
+
+
 
 }
-
