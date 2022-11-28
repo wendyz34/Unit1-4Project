36b0bc4f9ea91ce7cc7080d35f5b6b79d0ae87d9
wendy <wendyz34@nycstudents.net>
2022-11-28 17:06:52 -0500

    print

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        boolean done = false;
+        //boolean done = false;
         //intro
         System.out.println("Hello, this is a budgeting application. You'll be asked a series of question when you're done type done. ");
         System.out.println("What is your monthly earning?");
